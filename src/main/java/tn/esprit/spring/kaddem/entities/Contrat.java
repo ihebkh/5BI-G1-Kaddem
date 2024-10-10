11a90bd82a261766e45dde19384d75eb32a858b5
ihebkh <khmiri.iheb@esprit.tn>
2024-10-10 22:41:39 +0100

    new update 2

--- src/main/java/tn/esprit/spring/kaddem/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Contrat.java
@@ -1,6 +1,6 @@
 package tn.esprit.spring.kaddem.entities;
 
-import lombok.ToString;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -8,6 +8,11 @@ import javax.persistence.*;
 
 @Entity
 @ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Contrat implements Serializable {
 
     @Id
@@ -30,84 +35,7 @@ public class Contrat implements Serializable {
     @ManyToOne(cascade = CascadeType.ALL)
     private Etudiant etudiant;
 
-    // Default constructor
-    public Contrat() {
-        // This constructor is required for JPA
-    }
 
-    // Constructor with specific fields
-    public Contrat(Date dateDebutContrat, Date dateFinContrat, Specialite specialite, Boolean archive, Integer montantContrat) {
-        this.dateDebutContrat = dateDebutContrat;
-        this.dateFinContrat = dateFinContrat;
-        this.specialite = specialite;
-        this.archive = archive;
-        this.montantContrat = montantContrat;
-    }
 
-    // Full constructor with all fields
-    public Contrat(Integer idContrat, Date dateDebutContrat, Date dateFinContrat, Specialite specialite, Boolean archive, Integer montantContrat) {
-        this.idContrat = idContrat;
-        this.dateDebutContrat = dateDebutContrat;
-        this.dateFinContrat = dateFinContrat;
-        this.specialite = specialite;
-        this.archive = archive;
-        this.montantContrat = montantContrat;
-    }
 
-    // Getters and setters for all fields
-    public Integer getIdContrat() {
-        return idContrat;
-    }
-
-    public void setIdContrat(Integer idContrat) {
-        this.idContrat = idContrat;
-    }
-
-    public Date getDateDebutContrat() {
-        return dateDebutContrat;
-    }
-
-    public void setDateDebutContrat(Date dateDebutContrat) {
-        this.dateDebutContrat = dateDebutContrat;
-    }
-
-    public Date getDateFinContrat() {
-        return dateFinContrat;
-    }
-
-    public void setDateFinContrat(Date dateFinContrat) {
-        this.dateFinContrat = dateFinContrat;
-    }
-
-    public Specialite getSpecialite() {
-        return specialite;
-    }
-
-    public void setSpecialite(Specialite specialite) {
-        this.specialite = specialite;
-    }
-
-    public Boolean getArchive() {
-        return archive;
-    }
-
-    public void setArchive(Boolean archive) {
-        this.archive = archive;
-    }
-
-    public Integer getMontantContrat() {
-        return montantContrat;
-    }
-
-    public void setMontantContrat(Integer montantContrat) {
-        this.montantContrat = montantContrat;
-    }
-
-    public Etudiant getEtudiant() {
-        return etudiant;
-    }
-
-    public void setEtudiant(Etudiant etudiant) {
-        this.etudiant = etudiant;
-    }
 }
