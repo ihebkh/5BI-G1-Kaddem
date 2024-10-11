6495bdeab936421f6ac48e09dccb5c95643ed195
ihebkh <khmiri.iheb@esprit.tn>
2024-10-11 15:53:24 +0100

    update5

--- src/main/java/tn/esprit/spring/kaddem/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/ContratServiceImpl.java
@@ -1,6 +1,5 @@
 package tn.esprit.spring.kaddem.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
