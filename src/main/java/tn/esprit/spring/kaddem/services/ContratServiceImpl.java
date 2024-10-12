29acc3e85e09efcdadb29fa0695734794ea83ba3
ihebkh <khmiri.iheb@esprit.tn>
2024-10-12 17:56:51 +0100

    new

--- src/main/java/tn/esprit/spring/kaddem/services/ContratServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/ContratServiceImpl.java
@@ -111,7 +111,7 @@ public class ContratServiceImpl implements IContratService{
 	}
 
 	public float getChiffreAffaireEntreDeuxDates(Date startDate, Date endDate){
-		float differenceInTime = (float)(endDate.getTime() - startDate.getTime());
+		float differenceInTime = endDate.getTime() - startDate.getTime();
 		float differenceInDays = (differenceInTime / (1000 * 60 * 60 * 24)) % 365;
 		float differenceInmonths =differenceInDays/30;
 		List<Contrat> contrats=contratRepository.findAll();
