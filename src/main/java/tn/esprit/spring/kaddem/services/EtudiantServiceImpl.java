ad7758f2580877b2af3286b4cf59b5b93f0501b3
Aymen ZOUARI <Zouari.Aymen@esprit-tn.com>
2024-10-16 00:00:06 +0100

    fix2

--- src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
@@ -1,18 +1,13 @@
 package tn.esprit.spring.kaddem.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import lombok.extern.slf4j.Slf4j;
-
-import tn.esprit.spring.kaddem.entities.Contrat;
 import tn.esprit.spring.kaddem.entities.Departement;
-import tn.esprit.spring.kaddem.entities.Equipe;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.NoSuchElementException;
 
