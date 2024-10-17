7ef1bfcf9e0f567042b404ff96c714b39f902b36
Aymen ZOUARI <Zouari.Aymen@esprit-tn.com>
2024-10-17 23:49:49 +0100

    fi9

--- src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/EtudiantServiceImplTest.java
@@ -1,10 +1,8 @@
 package tn.esprit.spring.kaddem;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
@@ -24,7 +22,7 @@ class EtudiantServiceImplTest {
 
     @InjectMocks
     private EtudiantServiceImpl etudiantService;
-    
+
 
 
     @Test
