c138bd8083b8e607dc34a177aa1d4483d05fa280
Aymen ZOUARI <Zouari.Aymen@esprit-tn.com>
2024-10-17 22:06:41 +0100

    testConstructorRemove

--- src/test/java/tn/esprit/spring/kaddem/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/DepartementServiceImplTest.java
@@ -115,20 +115,6 @@ class DepartementServiceImplTest {
         verify(etudiantRepository, never()).save(any(Etudiant.class));
     }
 
-    @Test
-    public void testAllArgsConstructor() {
-        // Given
-        String nomDep = "Computer Science";
-        Integer idDep = 3;
-
-        // When
-        Departement departement = new Departement(idDep, nomDep);
-
-        // Then
-        assertEquals(idDep, departement.getIdDepart());
-        assertEquals(nomDep, departement.getNomDepart());
-    }
-
 
 
 
