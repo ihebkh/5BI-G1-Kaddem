92034b2c5956e00d12f3251f1b2b5f87fb7b2cb8
Aymen ZOUARI <Zouari.Aymen@esprit-tn.com>
2024-10-19 18:50:12 +0100

    appTest

---
+++ src/test/java/tn/esprit/spring/kaddem/KaddemApplicationTests.java
@@ -0,0 +1,11 @@
+package tn.esprit.spring.kaddem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KaddemApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
