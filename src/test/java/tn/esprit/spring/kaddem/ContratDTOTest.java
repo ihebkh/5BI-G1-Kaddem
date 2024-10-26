81c44c181ca9a238a442fd906b257c6a6b1b541e
ihebkh <khmiri.iheb@esprit.tn>
2024-10-26 16:05:36 +0100

    correction

---
+++ src/test/java/tn/esprit/spring/kaddem/ContratDTOTest.java
@@ -0,0 +1,34 @@
+package tn.esprit.spring.kaddem;
+
+import org.junit.jupiter.api.Test;
+import tn.esprit.spring.kaddem.entities.ContratDTO;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContratDTOTest {
+    @Test
+    public void testGettersAndSetters() {
+        ContratDTO contratDTO = new ContratDTO();
+
+        String dateDebut = "2024-10-26";
+        String dateFin = "2025-10-26";
+        String specialite = "INFORMATIQUE";
+        Boolean archive = false;
+        Integer montant = 1500;
+        Integer etudiantId = 1;
+
+        // Tester les setters
+        contratDTO.setDateDebutContrat(dateDebut);
+        contratDTO.setDateFinContrat(dateFin);
+        contratDTO.setSpecialite(specialite);
+        contratDTO.setArchive(archive);
+        contratDTO.setMontantContrat(montant);
+        contratDTO.setEtudiantId(etudiantId);
+
+        assertEquals(dateDebut, contratDTO.getDateDebutContrat());
+        assertEquals(dateFin, contratDTO.getDateFinContrat());
+        assertEquals(specialite, contratDTO.getSpecialite());
+        assertEquals(archive, contratDTO.getArchive());
+        assertEquals(montant, contratDTO.getMontantContrat());
+        assertEquals(etudiantId, contratDTO.getEtudiantId());
+    }
+}
