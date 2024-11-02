144b16920c3820534560d0a549ce5e402ac5f83f
Aymen ZOUARI <Zouari.Aymen@esprit-tn.com>
2024-11-02 05:08:49 +0100

    nchlh tekhdem4

--- src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
-import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Etudiant;
 import tn.esprit.spring.kaddem.repositories.DepartementRepository;
 import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
