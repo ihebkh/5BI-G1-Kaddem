af294a157e8127c985cbb1e9155adce15ac564c2
ahmedlaifia <laifia.ahmed@esprit.tn>
2024-11-07 18:58:21 +0100

    coverage_improvement_0.6

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Set;
 
 @RestController
-@AllArgsConstructor
+//@AllArgsConstructor
 @RequestMapping("/universite")
 public class UniversiteRestController {
 
