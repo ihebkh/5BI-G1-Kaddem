b3b39ee0edf89727cbc46893b6c83b679026918b
ahmedlaifia <laifia.ahmed@esprit.tn>
2024-11-07 16:47:55 +0100

    rabbi yostr

--- src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/UniversiteRestController.java
@@ -2,6 +2,8 @@ package tn.esprit.spring.kaddem.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Departement;
 import tn.esprit.spring.kaddem.entities.Universite;
@@ -14,52 +16,66 @@ import java.util.Set;
 @AllArgsConstructor
 @RequestMapping("/universite")
 public class UniversiteRestController {
+
 	@Autowired
-	IUniversiteService universiteService;
-	// http://localhost:8089/Kaddem/universite/retrieve-all-universites
+	private IUniversiteService universiteService;
+
+	// Endpoint to retrieve all universities
 	@GetMapping("/retrieve-all-universites")
-	public List<Universite> getUniversites() {
+	public ResponseEntity<List<Universite>> getAllUniversites() {
 		List<Universite> listUniversites = universiteService.retrieveAllUniversites();
-		return listUniversites;
+		return new ResponseEntity<>(listUniversites, HttpStatus.OK);
 	}
-	// http://localhost:8089/Kaddem/universite/retrieve-universite/8
+
+//http://localhost:8089/kaddem/universite/retrieve-universite/1
 	@GetMapping("/retrieve-universite/{universite-id}")
-	public Universite retrieveUniversite(@PathVariable("universite-id") Integer universiteId) {
-		return universiteService.retrieveUniversite(universiteId);
+	public ResponseEntity<Universite> getUniversiteById(@PathVariable("universite-id") Integer universiteId) {
+		Universite universite = universiteService.retrieveUniversite(universiteId);
+		if (universite != null) {
+			return new ResponseEntity<>(universite, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
-	// http://localhost:8089/Kaddem/universite/add-universite
+	// Endpoint to add a new university
 	@PostMapping("/add-universite")
-	public Universite addUniversite(@RequestBody Universite u) {
-		Universite universite = universiteService.addUniversite(u);
-		return universite;
+	public ResponseEntity<Universite> createUniversite(@RequestBody Universite universite) {
+		Universite savedUniversite = universiteService.addUniversite(universite);
+		return new ResponseEntity<>(savedUniversite, HttpStatus.CREATED);
 	}
 
-	// http://localhost:8089/Kaddem/universite/remove-universite/1
+	// Endpoint to remove a university by its ID
 	@DeleteMapping("/remove-universite/{universite-id}")
-	public void removeUniversite(@PathVariable("universite-id") Integer universiteId) {
-		universiteService.deleteUniversite(universiteId);
+	public ResponseEntity<Void> removeUniversite(@PathVariable("universite-id") Integer universiteId) {
+		universiteService.removeUniversite(universiteId);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
-	// http://localhost:8089/Kaddem/universite/update-universite
+	// Endpoint to update an existing university
 	@PutMapping("/update-universite")
-	public Universite updateUniversite(@RequestBody Universite u) {
-		Universite u1= universiteService.updateUniversite(u);
-		return u1;
+	public ResponseEntity<Universite> updateUniversite(@RequestBody Universite universite) {
+		Universite updatedUniversite = universiteService.updateUniversite(universite);
+		if (updatedUniversite != null) {
+			return new ResponseEntity<>(updatedUniversite, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 
-	//@PutMapping("/affecter-etudiant-departement")
-	@PutMapping(value="/affecter-universite-departement/{universiteId}/{departementId}")
-	public void affectertUniversiteToDepartement(@PathVariable("universiteId") Integer universiteId, @PathVariable("departementId")Integer departementId){
+	// Endpoint to assign a department to a university
+	@PutMapping("/affecter-universite-departement/{universiteId}/{departementId}")
+	public ResponseEntity<Void> assignUniversiteToDepartement(
+			@PathVariable("universiteId") Integer universiteId,
+			@PathVariable("departementId") Integer departementId) {
 		universiteService.assignUniversiteToDepartement(universiteId, departementId);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	@GetMapping(value = "/listerDepartementsUniversite/{idUniversite}")
-	public Set<Departement> listerDepartementsUniversite(@PathVariable("idUniversite") Integer idUniversite) {
-
-		return universiteService.retrieveDepartementsByUniversite(idUniversite);
+	// Endpoint to list departments of a specific university
+	@GetMapping("/listerDepartementsUniversite/{idUniversite}")
+	public ResponseEntity<Set<Departement>> getDepartementsByUniversite(@PathVariable("idUniversite") Integer idUniversite) {
+		Set<Departement> departements = universiteService.retrieveDepartementsByUniversite(idUniversite);
+		return new ResponseEntity<>(departements, HttpStatus.OK);
 	}
-
 }
-
-
